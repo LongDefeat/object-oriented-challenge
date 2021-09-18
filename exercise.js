ea5e1926e3abf3c14d0dc71483457b847642271f
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2021-09-18 16:33:59 -0700

    html and javascript for exercises
    This exercise helped me better understand classes and how they work/can be used to enhance storing objects in JS. While this is my first step into OOP, I'm really liking the structure.

---
+++ exercise.js
@@ -0,0 +1,76 @@
+/* ========PART ONE============*/
+// Create a class for vehicle. Each vehicle instance should have the following properties:
+
+// make
+// model
+// year
+// Each vehicle instance should have access to a method called honk, which returns the string “Beep.”
+
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  honk() {
+    console.log("Beep!");
+  }
+  toString() {
+    console.log(
+      `The vehicle is a ${this.make} ${this.model} from ${this.year}.`
+    );
+  }
+}
+
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+
+/* ========PART TWO============*/
+// Create a class for a car. The Car class should inherit from Vehicle and each car instance should have a property called numWheels which has a value of 4.
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+
+/* ========PART THREE============*/
+// Create a class for a Motorcycle. This class should inherit from Vehicle and each motorcycle instance should have a property called numWheels which has a value of 2. It should also have a revEngine method which returns “VROOM!!!”
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+  revEngine() {
+    console.log("VRROOOOOM!");
+  }
+}
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+/* ========PART FOUR============*/
+// Create a class for a Garage. It should have a property called vehicles which will store an array of vehicles, and a property called capacity which is a number indicating how many vehicles will fit in the garage. When you create a garage, vehicles will always be empty; you only need to provide the capacity.
+
+// A garage should also have an add method, which attempts to add a vehicle to the array of vehicles. However, if you try to add something which is not a vehicle, the garage should return the message “Only vehicles are allowed in here!”. Also, if the garage is at capacity, it should say “Sorry, we’re full.”
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (this.vehicles >= this.capacity) {
+      return "Sorry, we are full!";
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle added!";
+  }
+}
+
+let garage = new Garage(2);
